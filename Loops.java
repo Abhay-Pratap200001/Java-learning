e0be44ab572995f407e6e29f23eca4e87041c516
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-08-05 09:30:21 +0530

    Laearn solve to patterns

--- Loops.java
+++ Loops.java
@@ -1,14 +1,80 @@
-import java.util.*;
-public class Loops {
-    public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-       int n = sc.nextInt();
+// import java.util.*;
+// public class Loops {
+//     public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
 
-       int sum = 0;
-       for(int i = 1; i <= n; i++){
-         sum = sum + i;
-       }
+//        int sum = 0;
+//        for(int i = 1; i <= n; i++){
+//          sum = sum + i;
+//        }
+//        System.out.println(sum);
+//     }
+// }
 
-       System.out.println(sum);
-    }
-}
+
+
+// public class Loops {
+//     public static void main(String[] args) {
+//       for(int i=1; i<=5; i++){
+//         for(int j=1; j<=5; j++){
+//             System.out.print(" * ");
+//         }
+//         System.out.println();
+//       }
+//     }
+// }
+
+
+
+// public class Loops {
+//     public static void main(String[] args) {
+//      int n = 4;
+//      int m = 5;
+
+//      for(int i = 1; i <=n; i++){
+//         for(int j = 1; j <= m; j++){
+//             if (i == 1 || j == 1 || i == n || j == m) {
+//                 System.out.print(" * ");
+//             }else{
+//                 System.out.print("   ");
+//             }
+//         }
+//         System.out.println();
+//      }
+//     }
+// }
+
+
+// public class Loops {
+//     public static void main(String[] args) {
+//      int m = 5;
+
+//      for(int i = 1; i <= m; i++){
+
+//         for(int j=1; j<=m-i; j++ ){
+//             System.out.print(" ");
+//         }
+//         for(int j = 1; j <= i; j++){
+//             System.out.print("*");
+//         }
+//         System.out.println();
+//      }
+//     }
+// }
+
+
+
+// public class Loops {
+//     public static void main(String[] args) {
+//      int m = 5;
+//      int n = 1;
+//      for(int i=1; i<=m; i++){
+//         for(int j=1; j<=i; j++){
+//             System.out.print(n + " ");
+//             n++;
+//         }
+//         System.out.println();
+//      }
+//     }
+// }
