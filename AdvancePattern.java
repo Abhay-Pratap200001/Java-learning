1bd775ef6e19ff44867c3223cc420ee0e4d9b281
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-08-13 11:50:02 +0530

    Solved Advanced Patterns in java

---
+++ AdvancePattern.java
@@ -0,0 +1,41 @@
+// public class AdvancePattern {
+//     public static void main(String[] args) {
+//         int n = 5;
+//         //first half
+
+//         for(int i=1; i<=n; i++){
+//             // left side of pattern
+//             for(int j=1; j<=i; j++){
+//                 System.out.print("*");
+//             }
+//             int spaces = 2*(n-i);
+
+//             for(int j=1; j<=spaces; j++){
+//                 System.out.print(" ");
+//             }
+
+//             for(int j=1; j<=i; j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+
+
+//          for(int i=n; i>=1; i--){
+//             // left side of pattern
+//             for(int j=1; j<=i; j++){
+//                 System.out.print("*");
+//             }
+//             int spaces = 2*(n-i);
+            
+//             for(int j=1; j<=spaces; j++){
+//                 System.out.print(" ");
+//             }
+
+//             for(int j=1; j<=i; j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
